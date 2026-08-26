75128a7ed582514196f0f6040f361d22edbdb96a
Mathias Correa <mathiascorrea2006@email.com>
2026-08-26 15:26:01 -0300

    Atualizei o portfólio, melhorei algumas coisas, add seção de objetivo profissional, coloquei acessibilidade para o dispositovs moveis, etc

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-let slideIndex = 0; // 
+let slideIndex = 0;
 const slides = document.querySelectorAll('.slide');
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('#menu-principal');
 
 function mudarSlide(movimento) {
-
     slides[slideIndex].classList.remove('ativa');
-
-
     slideIndex += movimento;
-
-
     if (slideIndex >= slides.length) {
         slideIndex = 0;
     } else if (slideIndex < 0) {
@@ -17,3 +14,15 @@ function mudarSlide(movimento) {
 
     slides[slideIndex].classList.add('ativa');
 }
+
+menuToggle.addEventListener('click', () => {
+    const menuAberto = menu.classList.toggle('menu-aberto');
+    menuToggle.setAttribute('aria-expanded', menuAberto);
+});
+
+menu.addEventListener('click', (evento) => {
+    if (evento.target.matches('a')) {
+        menu.classList.remove('menu-aberto');
+        menuToggle.setAttribute('aria-expanded', 'false');
+    }
+});
